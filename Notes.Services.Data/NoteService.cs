ed5feb07588c959c1db3aa9e5a5761de5032bf2b
Ivaylo Hasov <ivailohasov@gmail.com>
2024-02-25 01:29:57 +0200

    Repo init
    Creating project and it's infrastructure.

---
+++ Notes.Services.Data/NoteService.cs
@@ -0,0 +1,13 @@
+﻿using Notes.Services.Data.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notes.Services.Data
+{
+    public class NoteService : INoteService
+    {
+    }
+}
