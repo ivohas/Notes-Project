489b2164d73e83fc0e33a3fcd28e8fc9e2630a87
Ivaylo Hasov <ivailohasov@gmail.com>
2024-02-25 16:38:57 +0200

    Data annotation

--- Notes.Services.Data/NoteService.cs
+++ Notes.Services.Data/NoteService.cs
@@ -1,13 +1,15 @@
-﻿using Notes.Services.Data.Interfaces;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Notes.Data;
+using Notes.Services.Data.Interfaces;
 
 namespace Notes.Services.Data
 {
     public class NoteService : INoteService
     {
+        private readonly NoteDbContext _dbContext;
+
+        public NoteService(NoteDbContext dbContext)
+        {
+            this._dbContext = dbContext;
+        }
     }
 }
--- Notes/Controllers/HomeController.cs
+++ Notes/Controllers/HomeController.cs
@@ -1,10 +1,18 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Notes.Services.Data.Interfaces;
 
 namespace Notes.Controllers
 {
     public class HomeController : BaseController
     {
+
+        private readonly INoteService _noteService;
+
+        public HomeController(INoteService noteService)
+        {
+            this._noteService = noteService;
+        }
         [AllowAnonymous]
         public IActionResult Index()
         {
